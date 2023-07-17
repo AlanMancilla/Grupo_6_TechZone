b63a22bc2bad14ca43d1556c3e726be488456f3d
AlvaroSaj <sajamaernestoalvaro@gmail.com>
2023-07-17 17:17:21 -0300

    app.js index.html y .gitignore?

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const path = require('path')
+
+app.use(express.static(path.resolve(__dirname,'./views')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '/.views/index.html'))
+})
+
+
+
+app.listen('3000', (req, res) => console.log('En linea'));
