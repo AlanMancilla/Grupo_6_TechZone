201462e25ce1557689be73e3748b74d655c4d3c3
InfernoDXL <theinferno224@gmail.com>
2023-11-04 20:52:40 -0300

    agregado de crud create

--- src/controllers/productDetailController.js
+++ src/controllers/productDetailController.js
@@ -5,112 +5,89 @@ const db = require("../database/models");
 const productsFilePath = path.join(__dirname, "../database/products.json");
 
 const productDetailControllers = {
-    productList: (req, res) => {
+  productList: (req, res) => {
+    db.Product.findAll().then((products) => {
+      res.render("products/list", { products });
+    });
+  },
+  productCreate: (req, res) => {
+    res.render("products/productcreate.ejs");
+  },
+  productDetail: (req, res) => {
+    db.Product.findOne({ where: { id: req.params.id } }).then((product) => {
+      res.render("products/productDetail.ejs", { product: product });
+    });
+  },
+  productCreatePost: (req, res) => {
+    let newProduct = {
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      image: req.file?.filename || "noImageFound.png",
+      price: Number(req.body.price),
+      discount: Number(req.body.discount),
+    };
 
-        db.Product.findAll()
-            .then(products => {
-                res.render('products/list', { products })
-            })
+    db.Product.create(newProduct).then((product) => {
+      res.redirect("/");
+    });
+  },
+  productEdit: (req, res) => {
+    db.Product.findOne({ where: { id: req.params.id } }).then((product) => {
+      res.render("products/productEdit.ejs", { product: product });
+    });
+  },
+  productEditPUT: (req, res) => {
+    let imageUpdate = "";
 
-    },
-    productCreate: (req, res) => {
-        res.render('products/productcreate.ejs')
-    },
-    productDetail: (req, res) => {
+    if (!req.file) {
+      imageUpdate = req.body.image_edit;
+    } else {
+      imageUpdate = req.file.filename;
+    }
 
-        db.Product.findOne({ where: { id: req.params.id }, })
-            .then(product => {
-                res.render('products/productDetail.ejs', { product: product })
-            })
+    let productEdit = {
+      id: Number(req.params.id),
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      image: imageUpdate,
+      price: Number(req.body.price),
+      discount: Number(req.body.discount),
+    };
 
-    },
-    productCreatePost: (req, res) => {
-
-        const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-
-        let new_id = products[products.length - 1].id + 1;
-        req.body.id = new_id;
-
-        let newProduct = req.body;
-        newProduct.image = req.file.filename;
-
-        products.push(newProduct);
-
-        fs.writeFile(productsFilePath, JSON.stringify(products), "utf-8", (err) => {
-            if (err) {
-                console.error("Error al escribir el archivo:", err);
-            } else {
-                console.log("Archivo sobrescrito exitosamente.");
-            }
-        });
-
-        res.render('products/productDetail.ejs', { product: newProduct });
-
-    },
-    productEdit: (req, res) => {
-
-        db.Product.findOne({ where: { id: req.params.id }, })
-            .then(product => {
-                res.render('products/productEdit.ejs', { product: product })
-            })
-
-    },
-    productEditPUT: (req, res) => {
-
-
-        let imageUpdate = "";
-
-        if (!req.file) {
-            imageUpdate = req.body.image_edit;
+    db.Product.update(productEdit, {
+      where: {
+        id: Number(req.params.id),
+      },
+    })
+      .then((product) => {
+        res.redirect(`/productDetail/${Number(req.params.id)}`);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+  productDELETE: (req, res) => {
+    db.Product.destroy({
+      where: {
+        id: Number(req.params.id),
+      },
+    })
+      .then((deletedRecord) => {
+        if (deletedRecord === 1) {
+          console.log("Producto eliminado exitosamente");
+          res.redirect("/");
         } else {
-            imageUpdate = req.file.filename;
+          console.log("Producto no encontrado");
+          res.status(404).send("Producto no encontrado");
         }
-
-        let productEdit = {
-            id: Number(req.params.id),
-            name: req.body.name,
-            description: req.body.description,
-            category: req.body.category,
-            image: imageUpdate,
-            price: Number(req.body.price),
-            discount: Number(req.body.discount),
-        };
-
-        db.Product.update(productEdit,
-            {
-                where: {
-                    id: Number(req.params.id)
-                }
-            })
-            .then(product => {
-                res.redirect(`/productDetail/${Number(req.params.id)}`);
-            })
-            .catch((error) => {
-                console.error(error);
-            })
-
-    },
-    productDELETE: (req, res) => {
-        db.Product.destroy({
-            where: {
-                id: Number(req.params.id),
-            },
-        })
-            .then((deletedRecord) => {
-                if (deletedRecord === 1) {
-                    console.log("Producto eliminado exitosamente");
-                    res.redirect("/");
-                } else {
-                    console.log("Producto no encontrado");
-                    res.status(404).send("Producto no encontrado");
-                }
-            })
-            .catch((error) => {
-                console.log("Ha ocurrido un error", error);
-                res.status(500).send("Ha ocurrido un error al eliminar el producto");
-            });
-    },
+      })
+      .catch((error) => {
+        console.log("Ha ocurrido un error", error);
+        res.status(500).send("Ha ocurrido un error al eliminar el producto");
+      });
+  },
 };
-
 
 module.exports = productDetailControllers;
